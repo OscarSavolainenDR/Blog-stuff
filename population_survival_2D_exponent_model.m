781826bd28b0b301db67cb508d0dd9a449b53b25
OscarSavolainen <67506963+OscarSavolainen@users.noreply.github.com>
2020-06-27 17:33:01 +0100

    Update population_survival_2D_exponent_model.m

--- population_survival_2D_exponent_model.m
+++ population_survival_2D_exponent_model.m
@@ -1,14 +1,18 @@
 function [P] = population_survival_2D_exponent_model(t,s_u,s_o,spread,points_sigma,theta,show_figures,figure_cutoff)
+     % Last edit: 27/06/2020; Oscar Savolainen.
+     % Function that produces a 2D map of decaying exponentials that are used to model humanity's chance of avoiding extinction.
+     % Each decay exponential is weighted using a log-normal distribution. They are then summed together to give the average weighted
+     % scenario. The final scenario is then weighted with a theta offset.
 
-    s = logspace(s_u-s_o*spread,s_u+s_o*spread,points_sigma);
+    s = logspace(s_u-s_o*spread,s_u+s_o*spread,points_sigma); % log normal scale of decaying exponents
     x = 1:length(s);
-    y = normpdf(x,length(s)/2,s_o*points_sigma/(2*spread));
+    y = normpdf(x,length(s)/2,s_o*points_sigma/(2*spread)); % log-normal distribution
     P = 0;
-    for i = 1:numel(s)
+    for i = 1:numel(s) % iterate through s (decaying exponential) scenarios
         p(i,1:figure_cutoff) = exp(-s(i)*(1:figure_cutoff)) * y(i);
         P = (exp(-s(i)*t) * y(i)) + P;
     end
-    P = (1-theta) * P + theta;
+    P = (1-theta) * P + theta; % theta offset
     
     if show_figures ==1
         figure,line(x,y); title('Probability distribution across s on a logarithmic scale')
