2c584198ab0a2af478b51a3b6b6f013e38ad380c
OscarSavolainen <67506963+OscarSavolainen@users.noreply.github.com>
2020-06-27 17:18:56 +0100

    Update exp_time_adjust.m

--- exp_time_adjust.m
+++ exp_time_adjust.m
@@ -1,6 +1,8 @@
 function [data_in] = exp_time_adjust(data_in,t_exp,f_exp)
-% This function multiplies the data coming in by the relevant expeerienced
-% time changes
+    % This function multiplies the data coming in by the relevant experienced
+    % time changes. E.g. if experienced time in VR speeds up by a factor of f_exp = 5
+    % at time step t_exp = 500, then the incoming data, from time step 500 onwards, 
+    % is multiplied by 5.
     data_in(t_exp:end) = data_in(t_exp:end) .* f_exp;
 end
 
